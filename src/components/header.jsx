5c0b495554dcb66270dd48411dbfe1a5e61dfc9d
Kevin <kevijens@hotmail.com>
2024-10-17 18:07:56 +0200

    Mock Header navigation
    + Mock navigation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
-import { Home } from "@mui/icons-material";
+import { Bed, Home, Login, ShoppingBag, Support } from "@mui/icons-material";
 import HeaderButton from "./header/headerButton";
-import "../styles/header.css"; 
+import "../styles/header.css";
 
 
 
@@ -10,8 +10,18 @@ import "../styles/header.css";
 
 export default function Header() {
     return (
-        <header className="nav-bar">
-            <HeaderButton Icon={Home} title="Home" to="/"/>
+        <header className="nav-bar flex-apart row stretch-width">
+            <div className="row">
+                <HeaderButton Icon={Home} title="Home" to="/" />
+                <HeaderButton Icon={Bed} title="Rooms" to="/rooms" />
+                <HeaderButton Icon={ShoppingBag} title="Order" to="/rooms" />
+            </div>
+            <div className="row">
+                <HeaderButton Icon={Support} title="Help" to="/" />
+                <HeaderButton Icon={Login} title="Login" to="/" />
+            </div>
+
+
         </header>
     )
 }
